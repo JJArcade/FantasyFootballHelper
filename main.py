444ef8352e5371385b4b9e46bc14f10b57daa9e5
jj <jjuarez@czarnowski.com>
2018-09-21 17:50:35 -0400

    stopping point

--- main.py
+++ main.py
@@ -39,6 +39,95 @@ def get_team_stats(soup, team):
                     temp_tag = "{}_{}".format("OPP", header)
                     temp_val = b.get_text()
                     out_json[temp_tag]=temp_val
+        else:
+            if header == "Rush-Yds-TDs":
+                for b in a.find_all("td"):
+                    cur_team = b.attrs["data-stat"]
+                    if main_team != cur_team:
+                        cur_team = "OPP"
+                    else:
+                        cur_team = team
+                    temp_tag = "{}_{}".format(cur_team, "Yds\\Rush")
+                    raw_val = b.get_text()
+                    raw_val = raw_val.split("-")
+                    ##CONVERT TO FLOAT
+                    for c in range(0,len(raw_val)):
+                        raw_val[c] = float(raw_val[c])
+                    temp_val = raw_val[1] / raw_val[0]
+                    out_json[temp_tag] = temp_val
+                    temp_tag = "{}_{}".format(cur_team, "Rush Attempts")
+                    temp_val = raw_val[0]
+                    out_json[temp_tag] = temp_val
+                    temp_tag = "{}_{}".format(cur_team, "Rush TDs")
+                    temp_val = raw_val[2]
+                    out_json[temp_tag] = temp_val
+            elif header == "Cmp-Att-Yd-TD-INT":
+                for b in a.find_all("td"):
+                    cur_team = b.attrs["data-stat"]
+                    if main_team != cur_team:
+                        cur_team = "OPP"
+                    else:
+                        cur_team = team
+                    temp_tag = "{}_{}".format(cur_team, "Cmp\\Attempt")
+                    raw_val = b.get_text()
+                    raw_val = raw_val.split("-")
+                    ##CONVERT TO FLOAT
+                    for c in range(0,len(raw_val)):
+                        raw_val[c] = float(raw_val[c])
+                    temp_val = raw_val[0] / raw_val[1]
+                    out_json[temp_tag] = temp_val
+                    temp_tag = "{}_{}".format(cur_team, "Passing TDs")
+                    temp_val = raw_val[3]
+                    out_json[temp_tag] = temp_val
+                    temp_tag = "{}_{}".format(cur_team, "Interceptions")
+                    temp_val = raw_val[4]
+                    out_json[temp_tag] = temp_val
+            elif header == "Sacked-Yards":
+                for b in a.find_all("td"):
+                    cur_team = b.attrs["data-stat"]
+                    if main_team != cur_team:
+                        cur_team = "OPP"
+                    else:
+                        cur_team = team
+                    temp_tag = "{}_{}".format(cur_team, "Sacked")
+                    raw_val = b.get_text()
+                    raw_val = raw_val.split("-")
+                    ##CONVERT TO FLOAT
+                    for c in range(0,len(raw_val)):
+                        raw_val[c] = float(raw_val[c])
+                    temp_val = raw_val[0]
+                    out_json[temp_tag] = temp_val
+            elif header == "Fumbles-Lost":
+                for b in a.find_all("td"):
+                    cur_team = b.attrs["data-stat"]
+                    if main_team != cur_team:
+                        cur_team = "OPP"
+                    else:
+                        cur_team = team
+                    temp_tag = "{}_{}".format(cur_team, "Fumbles")
+                    raw_val = b.get_text()
+                    raw_val = raw_val.split("-")
+                    ##CONVERT TO FLOAT
+                    for c in range(0,len(raw_val)):
+                        raw_val[c] = float(raw_val[c])
+                    temp_val = raw_val[0]
+                    out_json[temp_tag] = temp_val
+            elif header == "Penalties-Yards":
+                for b in a.find_all("td"):
+                    cur_team = b.attrs["data-stat"]
+                    if main_team != cur_team:
+                        cur_team = "OPP"
+                    else:
+                        cur_team = team
+                    temp_tag = "{}_{}".format(cur_team, "Fumbles")
+                    raw_val = b.get_text()
+                    raw_val = raw_val.split("-")
+                    ##CONVERT TO FLOAT
+                    for c in range(0,len(raw_val)):
+                        raw_val[c] = float(raw_val[c])
+                    temp_val = raw_val[0]
+                    out_json[temp_tag] = temp_val
+
     print(out_json)
     return out_json
 
@@ -46,5 +135,5 @@ def get_team_stats(soup, team):
 res = requests.get(main_url+phi_2017)
 table_grab = find_team_stats(res.text)
 table_soup = bs4.BeautifulSoup(table_grab, "html.parser")
-get_team_stats(table_soup, "PHI")
-
+data_dump = get_team_stats(table_soup, "PHI")
+print(json.dumps(data_dump, sort_keys=True, indent=4))
